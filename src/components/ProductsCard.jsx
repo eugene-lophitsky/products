7abd416a6ecd43050ff47ac15ecd8c0182402353
aleksey-t <megapozitivizm@yandex.ru>
2024-01-16 01:07:00 +0300

    add to cart functionality

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import "./product-card.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const ProductsCard = (props) => {
-  return <div className="product-card">
-    <div className="title">{props.title}</div>
-    <div className="description">{props.description}</div>
-    <div className="price">{props.price}</div>
-  </div>
+  const handleClick = () => {
+    fetch("http://localhost:8080/cart", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id: props.id,
+        title: props.title,
+        description: props.description,
+        price: props.price,
+      }),
+    }).then((res) => {
+      console.log(res.json());
+    });
+  };
+
+  return (
+    <div className="product-card">
+      <div className="title">{props.title}</div>
+      <div className="description">{props.description}</div>
+      Цена: <div className="price">{props.price}</div>
+      {props.showAddButton && (
+        <div>
+          <button onClick={handleClick}>Добавить в корзину</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 ProductsCard.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   price: PropTypes.number,
-}
+  showAddButton: PropTypes.bool,
+};
 
 export default ProductsCard;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,27 +2,34 @@ import React, { useEffect, useState } from "react";
 import ProductsCard from "../components/ProductsCard";
 
 const Catalog = () => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/products/cakes")
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products));
+  }, []);
 
-    useEffect(()=>{
-        fetch('http://localhost:8080/products/cakes')
-  .then(response => response.json())
-  .then(data => setProducts(data.products));
-    }, [])
+  return (
+    <>
+      <h1>Каталог товаров</h1>
 
-    return (
-       <>
-        <h1>Каталог товаров</h1>
-        
-        <div className="product-list">
-        {products.map(item =>{
-            return <ProductsCard title={item.title} description ={item.description} price={item.price} key={item._id}/>
+      <div className="product-list">
+        {products.map((item) => {
+          return (
+            <ProductsCard
+              title={item.title}
+              description={item.description}
+              price={item.price}
+              key={item._id}
+              showAddButton={true}
+              id={item._id}
+            />
+          );
         })}
-        </div>
-        </>
-        
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Catalog;
